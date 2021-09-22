0921779dafd9d4dbe62d5fb33b64a7c400f520db
Requi <privat@requi-dev.de>
2021-09-22 03:52:58 +0200

    Version Bump.

--- RPCSanity/RPCSanity.cs
+++ RPCSanity/RPCSanity.cs
@@ -18,7 +18,7 @@ namespace RPCSanity
         public const string Name = "RPCSanity";
         public const string Author = "Requi";
         public const string Company = null;
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string DownloadLink = "https://github.com/RequiDev/RPCSanity/";
     }
 
